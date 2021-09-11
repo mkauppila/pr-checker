8002d2f1af37b0c9471752f5d619e0c92d59136c
Markus Kauppila <markus.kauppila@gmail.com>
2021-09-11 12:48:22 +0300

    Add initial draft

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/google/go-github/v39/github"
+	"golang.org/x/oauth2"
+)
+
+func authenticatedGithubClient(accessToken string) *github.Client {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: accessToken},
+	)
+	tc := oauth2.NewClient(context.Background(), ts)
+
+	return github.NewClient(tc)
+}
+
+type RepoPrs struct {
+	RepoName string
+	Pulls    []*github.PullRequest
+}
+
+func main() {
+	var (
+		accessToken string
+		orgName     string
+	)
+	flag.StringVar(&accessToken, "token", "", "Github access token")
+	flag.StringVar(&orgName, "org-name", "", "Organization name")
+	flag.Parse()
+
+	if len(accessToken) == 0 || len(orgName) == 0 {
+		fmt.Println("Missing command line args")
+		fmt.Println("Run: pr-checker --help")
+		os.Exit(0)
+	}
+
+	client := authenticatedGithubClient(accessToken)
+
+	ctx := context.Background()
+
+	orgs, _, _ := client.Organizations.ListOrgMemberships(ctx, nil)
+	hasAccess := false
+	for _, org := range orgs {
+		if *org.Organization.Login == orgName {
+			hasAccess = true
+			break
+		}
+		b, _ := json.MarshalIndent(org, "", " ")
+		fmt.Println(string(b) + "\n")
+	}
+	if hasAccess {
+		fmt.Println("Access to org: ", orgName)
+	} else {
+		fmt.Println("No access to org: ", orgName)
+		os.Exit(1)
+	}
+
+	repos, _, err := client.Repositories.ListByOrg(ctx, orgName, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	var repoPrs []RepoPrs
+	for _, repo := range repos {
+		fmt.Println(*repo.Name + "\n")
+		pulls, _, err := client.PullRequests.List(ctx, orgName, *repo.Name, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		t := RepoPrs{RepoName: *repo.Name, Pulls: pulls}
+		repoPrs = append(repoPrs, t)
+	}
+
+	for _, r := range repoPrs {
+		hasFreshPrs := false
+		for _, pull := range r.Pulls {
+			if isFreshPullRequest(pull) {
+				hasFreshPrs = true
+				break
+			}
+		}
+		if !hasFreshPrs || len(r.Pulls) == 0 {
+			continue
+		}
+
+		fmt.Println(r.RepoName)
+		for _, pull := range r.Pulls {
+			if !isFreshPullRequest(pull) {
+				continue
+			}
+
+			title := *pull.Title
+			by := *pull.User.Login
+			link := *pull.HTMLURL
+			rstate := "OK"
+			if *pull.Draft {
+				rstate = "Draft"
+			}
+
+			fmt.Println("  ", rstate+" - "+by+" -> "+title)
+			fmt.Println("    ", link)
+		}
+	}
+}
+
+func isFreshPullRequest(pr *github.PullRequest) bool {
+	return pr.UpdatedAt.After(time.Now().AddDate(0, 0, -14))
+}
